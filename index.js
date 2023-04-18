a9c0760a242aaac4c9d1371b39e13e1e0cfdce85
triplettpatrick <patrick_triplett@alumni.brown.edu>
2023-04-18 01:05:41 -0400

    updated Progress Circle remote repo

---
+++ index.js
@@ -0,0 +1,12 @@
+let number = document.getElementById("number");
+let counter = 0;
+
+setInterval(() => {
+    if (counter == 87) {
+        clearInterval();
+    } else {
+        counter += 1;
+        number.innerHTML = counter + "%";
+    }
+
+}, 24);
